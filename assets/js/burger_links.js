833b60b862b5abe2b31f626484f0f314e112a0a2
medrayentn <medrayenelmi@gmail.com>
2025-07-17 12:54:31 +0100

    Delete assets directory

--- assets/js/burger_links.js
+++
@@ -1,8 +0,0 @@
-//--------------- Responsive nav links
-let btnMenu = document.getElementById("btnMenu");
-let navLinks = document.querySelector(".navigation");
-
-btnMenu.onclick = function () {
-    btnMenu.classList.toggle("fa-times");
-    navLinks.classList.toggle("active");
-};
